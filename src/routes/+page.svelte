340f22eb242ba70f73ba36533b37aed93477de5a
Melih Ulker <mku1000@usnh.edu>
2023-03-06 16:11:30 -0500

    first commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,34 @@
+<script>
+  import Counter from '../lib/Counter.svelte'
+  import unhmLogo from '/logo_unhm.svg'
+</script>
+
+<main>
+  <div>
+    <a href="https://manchester.unh.edu/" target="_blank" rel="noreferrer">
+      <img src= {unhmLogo} class="logo svelte" alt ="UNHM Logo"/>
+    </a>
+  </div>
+  <h1>Svelte</h1>
+
+  <div class="card">
+    <Counter />
+  </div>
+
+</main>
+
+<style>
+  .logo {
+    height: 6em;
+    padding: 1em;
+    will-change: filter;
+    transition: filter 300ms;
+  }
+  .logo:hover {
+    filter: drop-shadow(0 0 2em #646cffaa);
+  }
+  .logo.svelte:hover {
+    filter: drop-shadow(0 0 2em #ff3e00aa);
+  }
+
+</style>
---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,15 @@
+<div class=" ">
+    <h2>About us</h2>
+    <p> paragraph</p>
+
+</div>
+
+<style>
+    .about{
+      text-align: center;
+      display: block;
+      margin: 20px auto;
+    }
+
+
+</style>
